713da01039c057c90244734458c640202692fb61
sangdi <1592807515@qq.com>
2022-03-19 10:57:45 +0800

    trfgh

---
+++ demo-first/src/Test.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id='app'>
+        <p>{{username}}</p>
+        <button @click="change">hhhhhh</button>
+    </div>
+</template>
+<script>
+
+
+export default{
+    data(){
+        return {
+            username:'zsss'
+        }
+    },
+    methods:{
+        change(){
+            this.username='www'
+        }
+    }
+}
+
+</script>
--- demo-first/src/main.js
+++ demo-first/src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Test from './Test.vue'
 Vue.config.productionTip = false
 
 new Vue({
   el:'#app',
-  render: h => h(App),
+  render: h => h(Test),
 })
