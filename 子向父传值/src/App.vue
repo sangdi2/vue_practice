abeca0ef363280b09d71d2c4fcad8b5b930a7b7d
sangdi <1592807515@qq.com>
2022-03-28 11:32:17 +0800

    trfg

---
+++ 子向父传值/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>app组件的值是{{num}}</h1>
+    
+    <count @numchang="getnum"></count>
+    
+  </div>
+</template>
+
+<script>
+import count from './components/count.vue'
+
+export default{
+  data(){
+      return {
+        num:0
+      }
+  },
+  components:{
+    count
+  },
+  methods:{
+    getnum(val){
+      this.num=val
+    }
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
---
+++ 子向父传值/src/components/count.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">count的值是{{count}}<br/>
+  <button @click="add">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  
+  
+  data(){
+    return {
+      count:0
+    }
+  },
+  methods:{
+    add(){
+      this.count++;
+      this.$emit('numchang',this.count)
+      console.log('hiji')
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
