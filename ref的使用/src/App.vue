f5615c4e99e84d41d4c774ba7fcf9badc3a8b063
sangdi <1592807515@qq.com>
2022-03-30 10:37:52 +0800

    trfg

---
+++ ref的使用/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <input type="text" v-if="flag" @blur="truee" ref="she">
+        <button v-else @click="changeflag">按钮</button>
+    </div>
+</template>
+
+<script>
+export default {
+      data(){
+        return {
+          flag:false
+        }
+      },
+      methods:{
+        changeflag(){
+          this.flag=true
+          this.$nextTick(()=>{
+            this.$refs.she.focus()
+          })
+        },
+        truee(){
+          this.flag=false
+        }
+      }
+}
+</script>
+
+<style>
+
+</style>
+
