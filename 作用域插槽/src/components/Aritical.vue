88b683e8cb0e43bac42d36fdf3467356c26b561b
sangdi <1592807515@qq.com>
2022-04-01 20:51:43 +0800

    jngyf

---
+++ 作用域插槽/src/components/Aritical.vue
@@ -0,0 +1,36 @@
+<template>
+   <div class="app">
+       <div class="title">
+           <slot name="title"></slot>
+       </div>
+       <div class="content">
+           <slot name="content"></slot>
+       </div>
+       <div class="author">
+           <slot name="author" msg="hello vue.js"></slot>
+       </div>
+   </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="less" scoped>
+   .app div {
+       width: 500px;
+       height: 250px;
+   }
+   .title {
+       background-color: pink;
+   }
+   .content {
+       background-color: rgb(176, 176, 218);
+   }
+   .author {
+       background-color: aquamarine;
+   }
+
+</style>
