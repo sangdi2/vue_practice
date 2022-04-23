3858d64b3047324ef1666aab06418b7f298d19b6
sangdi <1592807515@qq.com>
2022-04-23 11:35:58 +0800

    gfbd

---
+++ 头条案例-vue2.0/src/views/Home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+      <van-nav-bar
+  title="首页" fixed
+  
+  
+/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+   .van-nav-bar{
+       background-color: #007bff;
+   }
+   /deep/.van-nav-bar__title {
+       color: white;
+   }
+</style>>
+
