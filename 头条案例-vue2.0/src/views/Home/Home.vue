1ba0b4542e7268f54f7820b4c4e5c9f0a3306bb1
sangdi <1592807515@qq.com>
2022-04-23 14:18:52 +0800

    hdf

--- 头条案例-vue2.0/src/views/Home/Home.vue
+++ 头条案例-vue2.0/src/views/Home/Home.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-      <van-nav-bar
-  title="首页" fixed
-  
-  
-/>
+      <van-nav-bar title="首页" fixed/>
+      <Aritical v-for="item in articallist" :key="item.id"></Aritical>
   </div>
 </template>
 
 <script>
+import request from '../../utils/request.js'
+import Aritical from '../../components/Artical/Articalinfo.vue'
 export default {
-
+  data(){
+     return {
+       page:1,
+       limit:10,
+       articallist:[]
+     }
+  },
+    methods:{
+      async initaritical(){
+        const {data:res}= await request.get('/articles',{params:{
+          _page:this.page,
+          _limit:this.limit
+        }})
+        console.log(res)
+        this.articallist=res
+      }
+    },
+    created(){
+      this.initaritical()
+    },
+    components:{
+      Aritical
+    }
 }
 </script>
 
